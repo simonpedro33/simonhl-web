8c152845133ebeff52310a14968d41de8184fe39
Simon Hernandez <simonpedro33@gmail.com>
2026-04-30 12:18:52 +0200

    setup: add deployment workflow

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'SimonHL';
+export const SITE_DESCRIPTION = 'Personal website of SimonHL, Personal Sovereignty Mastery, and more.';
